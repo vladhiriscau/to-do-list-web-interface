8647d128687344b54633b1a14865d9aa23ac5f86
vladhiriscau <vlad.hiriscau@yahoo.com>
2020-02-12 19:29:25 +0200

    displaying tasks from api

--- js/tasks.js
+++ js/tasks.js
@@ -9,20 +9,42 @@ window.ToDoList = {
         }).done(function (response){
             console.log(response);
 
+            ToDoList.displayTasks(JSON.parse(response))
             })
 
     },
 
     getTaskRow: function (task) {
+        // spread operator (...)
+        let formattedDeadline =
+            new Date(...task.deadline).toLocaleDateString ("ro");
+
+        let checkedAttribute = task.done ? " checked" : "";
+
+        //same resoult as with the ternart operator above
+        //if (tasks.done) {
+        // chackedAttribute = "cheched"
+        // } else {
+        // checkedAttribute = "";
+
         return `<tr>
             <td>${task.description}</td>
-            <td>${task.description}</td>
-            <td><input type="checkbox" data-id=${task.description} class="mark-done"/></td>
+            <td>${formattedDeadline}</td>
+            <td><input type="checkbox" data-id=${task.description} class="mark-done" ${checkedAttribute}/></td>
             <td><a href="#" data-id=${task.description} class="delete-task"><i class="fas fa-trash-alt"></i></a> </td>
         </tr>
 
         <tr>`
 
+    },
+
+    displayTasks: function (tasks) {
+        // week-typed (javascript) vs strong-typed (java)
+        var tableBody = '';
+
+        tasks.forEach(task => tableBody +=ToDoList.getTaskRow(task));
+
+        $("#tasks-table tbody").html(tableBody);
     }
 
 };
